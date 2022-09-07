197182166740c40b2dbf6b081204dfcb5db9b15c
Fernando <gamboaf011@gmail.com>
2022-09-07 11:19:30 -0700

    Started parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,30 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  // ex input. '[{key: "hello"}, {key: "hello"}]'
+  // split (,) = ['{key: "hello"}', '{key: "hello"}']
+  //{key: "hello"}
+  // SOLUTIONS:
+  // 1) iterate through input string
+  // 2)
+
+
+
+
+  // iterate through string
+
+    // if obj value equals [ is an array
+      // split string to convert array
+
+    // if obj value is { is an object
+
+    // if obj value is " its a string
+
+    //number
+
+    //boolean
+
+    //null
+
+    //
 };
